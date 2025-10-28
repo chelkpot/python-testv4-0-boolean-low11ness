923da420c6747e3d267e76d949646400dfd9020d
low11ness <torabata144@gmail.com>
2025-10-28 20:52:04 +0500

    dz

--- tasks/task1.py
+++ tasks/task1.py
@@ -2,7 +2,9 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
+    a, b, c = map(int, input().split())
+    result = a == b == c
+    print(result)
     
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
--- tasks/task6.py
+++ tasks/task6.py
@@ -3,7 +3,9 @@
 def solve():
 # Ниже пишите решение задачи
 
-   
+   a, b, c = map(int,input().split())
+   result = (a*a == b*b + c*c) or (b*b == a*a + c*c) or (c*c == a*a + b*b)
+   print(result)
    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
